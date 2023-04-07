0f7825ab751162913699390f1a2b4d4091cc32a9
Wation <Github@Wation.net>
2023-04-07 20:39:11 +0330

    Initial Commit

--- ci/ping-regex2.php
+++ ci/ping-regex2.php
@@ -1,8 +1,10 @@
-<pre>
 <?php
     if(!preg_match('/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\$/', $_GET['ip'])){
         header('Location: /ci/403.php');
     }
+?>
+<pre>
+<?php
     system("ping -c 4 ". $_GET['ip']);
 ?>
 </pre>
